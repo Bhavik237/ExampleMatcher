954204a9d6fb951d4088e75bdf8a8c3769528bf4
Bhavik237 <bhavikdpatel237@gmail.com>
2024-09-09 18:29:01 +0530

    Start Project

---
+++ examplematcher/src/main/java/com/api/examplematcher/ExamplematcherApplication.java
@@ -0,0 +1,13 @@
+package com.api.examplematcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamplematcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamplematcherApplication.class, args);
+	}
+
+}
---
+++ examplematcher/src/test/java/com/api/examplematcher/ExamplematcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.examplematcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamplematcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
